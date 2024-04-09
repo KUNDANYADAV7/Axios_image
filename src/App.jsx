ad92a044e7c5763fea29ec77631679a786a93983
KUNDANYADAV7 <kdyd999@gmail.com>
2024-04-09 16:16:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageUpload from "./image";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <>
+      <ImageUpload />
+    </>
+  );
+};
+
+export default App;
